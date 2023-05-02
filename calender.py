7946c8635e283aebbbc29adc6aa81b89be108522
pochii <johnmwanikig2000@gmail.com>
2023-05-02 13:55:36 +0300

    added custom tkinter

--- calender.py
+++ calender.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import customtkinter 
 import calendar
 root = tk.Tk()
 
@@ -15,7 +16,8 @@ entry.insert(0, "enter year")
 entry.pack()
 
 # Create the Submit button
-submit_button = tk.Button(root, text="Submit", command=display_input)
+
+submit_button = customtkinter.CTkButton(master=root, text="Submit", corner_radius=10,command=display_input)
 submit_button.pack()
 
 # Create the label to display the user's input
@@ -25,7 +27,21 @@ label.pack()
 root.mainloop()
 
 
+# import tkinter
+# import customtkinter  # <- import the CustomTkinter module
 
+# root_tk = tkinter.Tk()  # create the Tk window like you normally do
+# root_tk.geometry("400x240")
+# root_tk.title("CustomTkinter Test")
+
+# def button_function():
+#     print("button pressed")
+
+# # Use CTkButton instead of tkinter Button
+# button = customtkinter.CTkButton(master=root_tk, corner_radius=10, command=button_function)
+# button.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
+
+# root_tk.mainloop()
 
 
 
